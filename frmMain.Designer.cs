f9257dc1eb3c29ebc57ae2c4b826422235297e8e
Xlithan <66184380+Xlithan@users.noreply.github.com>
2022-02-14 06:27:05 +0000

    Initial commit

---
+++ frmMain.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace IndieUpdater
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.lblUpdate = new System.Windows.Forms.Label();
+            this.lblPlay = new System.Windows.Forms.Label();
+            this.txtText = new System.Windows.Forms.RichTextBox();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblUpdate
+            // 
+            this.lblUpdate.BackColor = System.Drawing.Color.Transparent;
+            this.lblUpdate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lblUpdate.Location = new System.Drawing.Point(18, 272);
+            this.lblUpdate.Name = "lblUpdate";
+            this.lblUpdate.Size = new System.Drawing.Size(65, 17);
+            this.lblUpdate.TabIndex = 0;
+            this.lblUpdate.Text = "Update";
+            this.lblUpdate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblUpdate.Click += new System.EventHandler(this.lblUpdate_Click);
+            // 
+            // lblPlay
+            // 
+            this.lblPlay.BackColor = System.Drawing.Color.Transparent;
+            this.lblPlay.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lblPlay.Location = new System.Drawing.Point(99, 272);
+            this.lblPlay.Name = "lblPlay";
+            this.lblPlay.Size = new System.Drawing.Size(65, 17);
+            this.lblPlay.TabIndex = 1;
+            this.lblPlay.Text = "Play";
+            this.lblPlay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblPlay.Click += new System.EventHandler(this.lblPlay_Click);
+            // 
+            // txtText
+            // 
+            this.txtText.BackColor = System.Drawing.Color.Black;
+            this.txtText.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtText.ForeColor = System.Drawing.Color.White;
+            this.txtText.Location = new System.Drawing.Point(13, 28);
+            this.txtText.Name = "txtText";
+            this.txtText.ReadOnly = true;
+            this.txtText.Size = new System.Drawing.Size(383, 228);
+            this.txtText.TabIndex = 2;
+            this.txtText.Text = "Connecting...";
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.BackColor = System.Drawing.Color.Transparent;
+            this.lblVersion.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lblVersion.Location = new System.Drawing.Point(15, 8);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(97, 13);
+            this.lblVersion.TabIndex = 3;
+            this.lblVersion.Text = "Current Revision: 0";
+            this.lblVersion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.AutoSize = true;
+            this.lblProgress.BackColor = System.Drawing.Color.Transparent;
+            this.lblProgress.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lblProgress.Location = new System.Drawing.Point(320, 272);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(80, 13);
+            this.lblProgress.TabIndex = 4;
+            this.lblProgress.Text = "Progress: 100%";
+            this.lblProgress.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lblProgress.Visible = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Fuchsia;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(412, 301);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.txtText);
+            this.Controls.Add(this.lblPlay);
+            this.Controls.Add(this.lblUpdate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Indie Updater";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUpdate;
+        private System.Windows.Forms.Label lblPlay;
+        private System.Windows.Forms.RichTextBox txtText;
+        private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.Label lblProgress;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Diagnostics;
+using Ionic.Zip;
+
+namespace IndieUpdater
+{
+    public partial class frmMain : Form
+    {
+        public int cVersion = 0;
+        public int sVersion = 0;
+        public string exePath = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+        public frmMain()
+        {
+            InitializeComponent();
+
+            var MyIni = new IniFile(exePath + "\\data files\\config.ini");
+
+            if (MyIni.KeyExists("Version", "UPDATER"))
+            {
+                cVersion = Int32.Parse(MyIni.Read("Version", "UPDATER"));
+                lblVersion.Text = "Current Revision: " + cVersion;
+            }
+
+            WebClient webClient = new WebClient();
+
+            txtText.AppendText("\nRetrieving revision...");
+
+            try
+            {
+                sVersion = Int32.Parse(webClient.DownloadString("http://website.com/updates/version.txt"));
+            }
+            catch
+            {
+                txtText.AppendText("\nCould not connect to update server.");
+                return;
+            }
+
+            if(sVersion < 0)
+            {
+                txtText.AppendText("\nCould not update (Invalid server revision).");
+                return;
+            }
+            
+            if (sVersion > cVersion)
+            {
+                lblPlay.Enabled = false;
+                txtText.AppendText("\nClient outdated. Click 'Update' to download the latest revision (Revision: " + sVersion + ").");
+            }
+            else
+            {
+                txtText.AppendText("\nYou have the latest revision (Revision: " + sVersion + ").");
+                lblPlay.Enabled = true;
+                lblUpdate.Enabled = false;
+            }
+        }
+
+        private void lblUpdate_Click(object sender, EventArgs e)
+        {
+            WebClient objWebClient = new WebClient();
+            lblProgress.Visible = true;
+            lblProgress.Text = "Progress: 0%";
+            try
+            {
+                txtText.AppendText("\nDownloading update...");
+                objWebClient.DownloadFileAsync(new Uri("http://website.com/updates/Patch" + sVersion + ".zip"), "Update.zip");
+            }
+            catch
+            {
+                txtText.AppendText("\nFailed to download update.");
+                return;
+            }
+            objWebClient.DownloadProgressChanged += (s, a) =>
+            {
+                lblProgress.Text = "Progress: " + a.ProgressPercentage + "%";
+            };
+            objWebClient.DownloadFileCompleted += (s, a) =>
+            {
+                txtText.AppendText("\nDownload completed.");
+                //lblProgress.Visible = false;
+
+                // Unzip the update file
+                using (ZipFile zip = ZipFile.Read("Update.zip"))
+                {
+                    txtText.AppendText("\nExtracting, please wait...");
+                    foreach (ZipEntry i in zip)
+                    {
+                        i.Extract(exePath, ExtractExistingFileAction.OverwriteSilently);
+                    }
+                }
+                File.Delete("Update.zip");
+
+            };
+            objWebClient.Dispose();
+
+            var MyIni = new IniFile(exePath + "\\data files\\config.ini");
+
+            if (MyIni.KeyExists("Version", "UPDATER"))
+            {
+                MyIni.Write("Version", "" + sVersion, "UPDATER");
+                lblVersion.Text = "Current Revision: " + sVersion;
+            }
+
+            txtText.AppendText("\nUpdate complete!");
+
+            lblUpdate.Enabled = false;
+            lblPlay.Enabled = true;
+        }
+
+        private void lblPlay_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "Game.exe";
+            Process.Start(startInfo);
+            Application.Exit();
+        }
+    }
+}
